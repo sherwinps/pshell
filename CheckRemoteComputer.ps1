55948ca429ed4f4fef1c77a8d5d677a12f9391b9
Sherwin P. Sanchez <sherwinpsanchez@gmail.com>
2017-11-30 10:15:16 -0500

    Add files via upload
    Created to check workstations that are still up and which ones are needed to be shutdown prior to a Scheduled Power Outage.
    Check all workstations listed on a text file (e.g. LIST.txt) and determine which ones are up/down.
    Creates 2 text files as output: ComputersToShutdown.txt, DownComputers.txt
    Default directory is set at C:\TEMP .

---
+++ CheckRemoteComputer.ps1
@@ -0,0 +1,13 @@
+﻿<#
+Script Name	: CheckRemoteComputer.ps1
+Description : Check if a remote computer is up.
+Author		: Sherwin Sanchez
+Last Update	: November 2017
+#>
+$Servers = (Get-Content C:\TEMP\WILDLIST.txt)
+
+foreach ($Server in $Servers)
+{
+	if (Test-Connection $Server -quiet) { "Computer $Server verified to be responding to ping at $(Get-Date)" | Add-Content -Path C:\TEMP\ComputersToShutdown.txt }
+	else { "Computer $Server unresponsive to ping at $(Get-Date)" | Add-Content -Path C:\TEMP\DownComputers.txt }
+}
