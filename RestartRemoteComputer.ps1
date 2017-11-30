8ab62f1d72240436b29a840032d607579de6e91a
Sherwin P. Sanchez <sherwinpsanchez@gmail.com>
2017-11-30 09:25:24 -0500

    Add files via upload
    Restart a remote computer.

---
+++ RestartRemoteComputer.ps1
@@ -0,0 +1,9 @@
+﻿<#
+Script Name	: RestartRemoteComputer.ps1
+Description : Restart a remote computer.
+Author		: Sherwin Sanchez
+Last Update	: October 2017
+#>
+
+$cname = Read-Host 'Enter Computer Name to restart :'
+Restart-Computer -Force -ComputerName $cname
