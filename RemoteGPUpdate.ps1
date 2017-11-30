f13adb73e18ff8aba38f1701dc90b32f148bb596
Sherwin P. Sanchez <sherwinpsanchez@gmail.com>
2017-11-30 09:22:32 -0500

    Add files via upload
    Initiate GPUpdate on a specified Remote Workstation.

---
+++ RemoteGPUpdate.ps1
@@ -0,0 +1,9 @@
+﻿<#
+Script Name	: RemoteGPUpdate.ps1
+Description : Initiate GPUpdate on a Remote Workstation.
+Author		: Sherwin Sanchez
+Last Update	: October 2017
+#>
+
+$cname = Read-Host 'Enter Computer Name to update :'
+Invoke-GPUpdate -Computer $cname -Force -RandomDelayInMinutes 0
