eefd25b13170757fc0df9d6660294fbc6546f300
Sherwin P. Sanchez <sherwinpsanchez@gmail.com>
2017-11-30 10:18:39 -0500

    Add files via upload
    Shutdown remote computers listed on a Text File.

---
+++ ShutdownRemoteComputers.ps1
@@ -0,0 +1,8 @@
+﻿<#
+Script Name	: ShutdownRemoteComputers.ps1
+Description : Shutdown remote computers listed on a Text File
+Author		: Sherwin Sanchez
+Last Update	: November 2017
+#>
+
+Stop-Computer -ComputerName (Get-Content C:\TEMP\THELIST.txt) -Force
