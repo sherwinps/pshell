7f2da7456fffcb76944f58e0b8c4cc9a98b18685
Sherwin P. Sanchez <sherwinpsanchez@gmail.com>
2017-11-30 09:23:20 -0500

    Update RemoteGPUpdate.ps1

--- RemoteGPUpdate.ps1
+++ RemoteGPUpdate.ps1
@@ -1,8 +1,6 @@
-﻿<#
+<#
 Script Name	: RemoteGPUpdate.ps1
 Description : Initiate GPUpdate on a Remote Workstation.
-Author		: Sherwin Sanchez
-Last Update	: October 2017
 #>
 
 $cname = Read-Host 'Enter Computer Name to update :'
