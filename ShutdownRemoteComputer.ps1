edee7a08cbe5e9a7005a60fcf95fcb70284cb4c8
Sherwin P. Sanchez <sherwinpsanchez@gmail.com>
2017-11-30 09:35:26 -0500

    Update ShutdownRemoteComputer.ps1

--- ShutdownRemoteComputer.ps1
+++ ShutdownRemoteComputer.ps1
@@ -1,8 +1,6 @@
-﻿<#
+<#
 Script Name	: ShutdownRemoteComputer.ps1
 Description : Shutdown a remote computer.
-Author		: Sherwin Sanchez
-Last Update	: November 2017
 #>
 
 $cname = Read-Host 'Enter Computer Name to shutdown :'
