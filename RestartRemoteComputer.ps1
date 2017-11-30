15fb6cc7bf9be090099194805a877b532efb10c5
Sherwin P. Sanchez <sherwinpsanchez@gmail.com>
2017-11-30 09:25:58 -0500

    Update RestartRemoteComputer.ps1

--- RestartRemoteComputer.ps1
+++ RestartRemoteComputer.ps1
@@ -1,8 +1,6 @@
-﻿<#
+<#
 Script Name	: RestartRemoteComputer.ps1
 Description : Restart a remote computer.
-Author		: Sherwin Sanchez
-Last Update	: October 2017
 #>
 
 $cname = Read-Host 'Enter Computer Name to restart :'
