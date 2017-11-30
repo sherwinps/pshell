915ef5ac5431ebf93546c39313ff0da055d97f19
Sherwin P. Sanchez <sherwinpsanchez@gmail.com>
2017-11-30 09:34:56 -0500

    Add files via upload
    Shutdown a remote computer.

---
+++ ShutdownRemoteComputer.ps1
@@ -0,0 +1,9 @@
+﻿<#
+Script Name	: ShutdownRemoteComputer.ps1
+Description : Shutdown a remote computer.
+Author		: Sherwin Sanchez
+Last Update	: November 2017
+#>
+
+$cname = Read-Host 'Enter Computer Name to shutdown :'
+Stop-Computer -ComputerName $cname -Force
