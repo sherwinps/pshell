88aed9d5adab03e523de24280887e8c74f881454
Sherwin P. Sanchez <sherwinpsanchez@gmail.com>
2017-11-30 09:20:53 -0500

    Update AppXPackageList.ps1

--- AppXPackageList.ps1
+++ AppXPackageList.ps1
@@ -1,8 +1,6 @@
 <#
 Script Name	: AppXPackageList.ps1
 Description : Displays Windows 10 Application Packages in an OutGridView via PassThru pipe
-Author		: Sherwin Sanchez
-Last Update	: September 2017
 #>
 
 Get-AppxPackage | Out-GridView -PassThru | Remove-AppxPackage
